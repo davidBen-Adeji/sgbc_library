a976907caa74f4b0c51fc2bf5442563e58125eee
davidBen-Adeji <benadejidavid@gmail.com>
2025-09-27 17:00:56 +0100

    modified book.model.ts

--- models/book.model.ts
+++ models/book.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import mongoose, { Schema, model, models, Model } from "mongoose";
 import { IBook } from "./book.types";
 
 const bookSchema = new Schema<IBook>(

b52f1921940a36aefe9cf0def4be1f39ccd4cd19
davidBen-Adeji <benadejidavid@gmail.com>
2025-09-29 17:26:55 +0100

    modified searchParamWatcher.tsx

--- app/search/_ui/searchParamWatcher.tsx
+++ app/search/_ui/searchParamWatcher.tsx
@@ -1,12 +1,21 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function SearchParamWatcher() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
   return (
-    <h1 className="w-minus-50 mx-auto mt-7 text-xl font-bold max-w-5xl">
-      Showing search results for {`"${query}"`}
-    </h1>
+    <Suspense
+      fallback={
+        <h1 className="w-minus-50 mx-auto mt-7 text-xl font-bold max-w-5xl">
+          Loading...
+        </h1>
+      }
+    >
+      <h1 className="w-minus-50 mx-auto mt-7 text-xl font-bold max-w-5xl">
+        Showing search results for {`"${query}"`}
+      </h1>
+    </Suspense>
   );
 }

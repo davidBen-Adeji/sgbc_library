105cf063e61e69733c9f50e2b51dd2eaab1ea03d
davidBen-Adeji <benadejidavid@gmail.com>
2025-09-29 17:31:30 +0100

    deleted searchParamWatcher.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -17,6 +17,9 @@ export default async function SearchPage({
 
   return (
     <>
+      <h1 className="w-minus-50 mx-auto mt-7 text-xl font-bold max-w-5xl">
+        Showing search results for {`"${query}"`}
+      </h1>
       {books.length === 0 ? (
         <NotFound />
       ) : (

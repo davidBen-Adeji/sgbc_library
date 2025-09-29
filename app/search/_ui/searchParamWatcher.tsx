40a74696e14feddd30b927b9e92c82eb477c7c71
davidBen-Adeji <benadejidavid@gmail.com>
2025-09-29 16:42:08 +0100

    modified searchParamWatcher.tsx

--- app/search/_ui/searchParamWatcher.tsx
+++ app/search/_ui/searchParamWatcher.tsx
@@ -6,7 +6,7 @@ export default function SearchParamWatcher() {
   const query = searchParams.get("query");
   return (
     <h1 className="w-minus-50 mx-auto mt-7 text-xl font-bold max-w-5xl">
-      Showing search results for "{query}"
+      Showing search results for {`"${query}"`}
     </h1>
   );
 }
